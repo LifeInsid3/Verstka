ac88f49f2dfe80f02d3a2be04929388689abd6ec
LifeInsid3 <stas.vlasenko.76@mail.ru>
2021-10-14 19:52:20 +0300

    1 part

---
+++ app/src/main/java/com/example/verstka/AuthoActivity.java
@@ -0,0 +1,13 @@
+package com.example.verstka;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AuthoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_autho);
+    }
+}
